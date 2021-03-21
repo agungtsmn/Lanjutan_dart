088e4e54cf920c0fd24fdd13db4434911b2fd5fb
agungtsmn <0812agung@gmail.com>
2021-03-21 20:38:52 +0700

    Knight

---
+++ dasar_pbo/lib/knight.dart
@@ -0,0 +1,8 @@
+// Amanda Agung Permata
+// 6304191217
+// Pemrograman Berorientasi Obyek
+
+import 'package:dasar_pbo/drink_ability_mixin.dart';
+import 'package:dasar_pbo/hero.dart';
+
+class Knight extends Hero with DrinkAbilityMixin {}
