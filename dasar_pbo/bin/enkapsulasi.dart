309062ac5550925f57d2c7d01aca441d4e4c6601
Amanda Agung Permata <79837189+agungtsmn@users.noreply.github.com>
2021-04-19 08:18:53 +0700

    Update enkapsulasi.dart

--- dasar_pbo/bin/enkapsulasi.dart
+++ dasar_pbo/bin/enkapsulasi.dart
@@ -1,51 +1,51 @@
-// Amanda Agung Permata
-// 6304191217
-// Pemrograman Berorientasi Obyek
+Amanda Agung Permata
+6304191217
+Pemrograman Berorientasi Obyek
 
-// --------------------------------------------
-// Enkapsulasi
+--------------------------------------------
+Enkapsulasi
 
-// import 'dart:io';
-// import 'package:dasar_pbo/persegi_panjang.dart';
+import 'dart:io';
+import 'package:dasar_pbo/persegi_panjang.dart';
 
-// main(List<String> args) {
-//   PersegiPanjang kotak1, kotak2;
-//   double luasKotak1;
+main(List<String> args) {
+  PersegiPanjang kotak1, kotak2;
+  double luasKotak1;
 
-//   kotak1 = new PersegiPanjang();
-//   kotak1.setPanjang(2);
-//   kotak1.lebar = -3;
+  kotak1 = new PersegiPanjang();
+  kotak1.setPanjang(2);
+  kotak1.lebar = -3;
 
-//   kotak2 = PersegiPanjang();
-//   kotak2.setPanjang(double.tryParse(stdin.readLineSync()));
-//   kotak2.lebar = double.tryParse(stdin.readLineSync());
+  kotak2 = PersegiPanjang();
+  kotak2.setPanjang(double.tryParse(stdin.readLineSync()));
+  kotak2.lebar = double.tryParse(stdin.readLineSync());
 
-//   luasKotak1 = kotak1.hitungLuas();
+  luasKotak1 = kotak1.hitungLuas();
 
-//   print(luasKotak1 + kotak2.hitungLuas());
-//   print(kotak1.lebar);
-// }
+  print(luasKotak1 + kotak2.hitungLuas());
+  print(kotak1.lebar);
+}
 
-// --------------------------------------------
-// Enkapsulasi 2
+--------------------------------------------
+Enkapsulasi 2
 
-// import 'dart:io';
-// import 'package:dasar_pbo/persegi_panjang.dart';
+import 'dart:io';
+import 'package:dasar_pbo/persegi_panjang.dart';
 
-// main(List<String> args) {
-//   PersegiPanjang kotak1, kotak2;
-//   double luasKotak1;
+main(List<String> args) {
+  PersegiPanjang kotak1, kotak2;
+  double luasKotak1;
 
-//   kotak1 = new PersegiPanjang();
-//   kotak1.setPanjang(2);
-//   kotak1.lebar = -3;
+  kotak1 = new PersegiPanjang();
+  kotak1.setPanjang(2);
+  kotak1.lebar = -3;
 
-//   kotak2 = PersegiPanjang();
-//   kotak2.setPanjang(double.tryParse(stdin.readLineSync()));
-//   kotak2.lebar = double.tryParse(stdin.readLineSync());
+  kotak2 = PersegiPanjang();
+  kotak2.setPanjang(double.tryParse(stdin.readLineSync()));
+  kotak2.lebar = double.tryParse(stdin.readLineSync());
 
-//   luasKotak1 = kotak1.luas;
+  luasKotak1 = kotak1.luas;
 
-//   print(luasKotak1 + kotak2.luas);
-//   print(kotak1.lebar);
-// }
+  print(luasKotak1 + kotak2.luas);
+  print(kotak1.lebar);
+}
